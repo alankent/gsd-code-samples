88e706dfa2ad9cc99b359ccd02ea98816dbcafd4
Alan Kent <alan.james.kent@gmail.com>
2016-03-05 16:40:31 -0800

    Examples for volume 2

---
+++ volume2-theme-web-page-assets/app/design/frontend/AlanKent.old/footer-image/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/AlanKent/footer-image',
+    __DIR__
+);
---
+++ volume2-theme-web-page-assets/app/design/frontend/AlanKent.old/new-logo/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/AlanKent/new-logo',
+    __DIR__
+);
